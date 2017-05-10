1303b8b8c1ed32a5fd9de71d82aa4f6475b74d00
Nathaniel Caprile <nathanielcaprile@Nathaniels-MacBook-Pro-2.local>
2017-05-10 01:09:05 -0700

    Change main file to add abc class format

--- Schillinger.java
+++ Schillinger.java
@@ -14,22 +14,35 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Schillinger {
   public static void main(String[] args) {
-    ArrayList<Character> music = new ArrayList<Character>();
-     music = getFile();
+     
+     //music = getFile();
+     abc music = getFile();
+     rhythm rhyme = new rhythm();
+     //System.out.println(music.getNotes());
      // call rhythm(music), harmony(music), melody(music), and counterpoint(music) here
+     ArrayList<Character> newNotes = new ArrayList<Character>();
+     newNotes = rhyme.reflection(music.getNotes());
+     music.setNotes(newNotes);
+     //System.out.println(music.getNotes());
+     writeFile(music);
+     System.out.println("\nFile called 'output.abc' has been created\n");
   }
   /*
     prompts user for file and opens the file
     then returns the contents of the file as a
     String array.
   */
-  public static ArrayList<Character> getFile(){
+  public static abc getFile(){
     ArrayList<Character> notes = new ArrayList<Character>();
+    abc newFile = new abc();
     Scanner console = new Scanner(System.in);
-    System.out.print("Enter a valid abc Music notation file.");
+    System.out.print("Enter a valid abc Music notation file.\n");
     String fileName = console.nextLine();
     File file = new File(fileName);
     BufferedReader fileReader = null;
@@ -37,10 +50,13 @@ public class Schillinger {
       fileReader = new BufferedReader(new FileReader(file));
       String fileContents = null;
       while ((fileContents = fileReader.readLine()) != null) {
-        int lineLen = fileContents.length();
+        /*int lineLen = fileContents.length();
         for(int i = 0; i < lineLen; i++) {
           notes.add(fileContents.charAt(i));
-        }
+        }*/
+        //System.out.println("About to add");
+        newFile.add(fileContents);
+        //System.out.println(".");
       }
     } catch (FileNotFoundException e) {
       e.printStackTrace();
@@ -54,7 +70,42 @@ public class Schillinger {
         } catch (IOException ioe) {
         }
     }
-    return notes;
+    return newFile;
   }
-
+  
+  public static void writeFile(abc file) {
+    BufferedWriter bufwrite = null;
+    FileWriter filewrite = null;
+    
+    try {
+    
+        filewrite = new FileWriter("output.abc");
+        bufwrite = new BufferedWriter(filewrite);
+        bufwrite.write(file.getHeader());
+        
+        ArrayList<Character> notes = file.getNotes();
+        String allNotes = "";
+        int size = notes.size();
+        for (int i = 0; i < size; i++) {
+            allNotes += notes.get(i);
+        }
+        bufwrite.write(allNotes);
+        
+    } catch (IOException e) {
+        e.printStackTrace();
+    } finally {
+        try {
+            if (bufwrite != null) {
+                bufwrite.close();
+            }
+            
+            if (filewrite != null) {
+                filewrite.close();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+  }
+            
 }
